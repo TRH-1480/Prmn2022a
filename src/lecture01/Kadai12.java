2c79a5bd2992ab1ec43bf4758374dbe60bd961fb
TRH-1480 <108605772+TRH-1480@users.noreply.github.com>
2022-11-01 16:36:49 +0900

    Kadai12

---
+++ src/lecture01/Kadai12.java
@@ -0,0 +1,8 @@
+package lecture01;
+
+public class Kadai12 {
+    public static void main(String[] args){
+        int studentNumber = 2211480;
+        System.out.println("b"+studentNumber+" Haruka Takahashi");
+    }
+}
