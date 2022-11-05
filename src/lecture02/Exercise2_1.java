0409c37bb14de1c81cdcae86ca99ec190551dc61
TRH-1480 <108605772+TRH-1480@users.noreply.github.com>
2022-11-05 15:16:49 +0900

    kadai21

---
+++ src/lecture02/Exercise2_1.java
@@ -0,0 +1,15 @@
+package lecture02;
+
+public class Exercise2_1 {
+    public static void main(String[] args) {
+        Human human = new Human();
+        human.name = "たかし";
+        human.age = 18;
+        human.print();
+
+        Human human1 = new Human();
+        human1.name = "ひろし";
+        human1.age = 21;
+        human1.print();
+    }
+}
